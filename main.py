0290590bf8a0dec3100a741e3eb2171bf4b890c7
Will Lynas <43895423+will-lynas@users.noreply.github.com>
2023-09-19 21:36:15 +0100

    Change to use popup

--- main.py
+++ main.py
@@ -13,16 +13,8 @@ def get_tmux_option(option: str, default: str) -> str:
     result = run_command(f"tmux show-option -gqv {option}")
     return result or default
 
-def split_mode() -> str:
-    # How to split the tmux window:
-    # 'vertical' - top/bottom split
-    # 'horizontal' - side-by-side split
-    window_mode = get_tmux_option("@tm-window-mode", "vertical")
-    mode_map = {"vertical": "v", "horizontal": "h"}
-    return mode_map[window_mode]
-
 if __name__ == "__main__":
     keybind = get_tmux_option("@tm-keybind", "e")
     inner_command = f"{INSTALL_DIR}/pane_main.py #{{pane_id}}"
-    command = f"tmux split-window -{split_mode()} \"{inner_command}\""
+    command = f"tmux display-popup -w80% -h80% -E \"{inner_command}\""
     run_command(f"tmux bind {keybind} run-shell '{command}'")
